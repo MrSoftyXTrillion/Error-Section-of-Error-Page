5bc8f0a748dce02fc1fb13b19b2e72274975419b
Mr.SoftyXTrillion <khaledbinnasir1714412140@gmail.com>
2023-07-29 18:20:42 +0600

    Design and Responsive done

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$('.slickSliderAnimation').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '.leftArrow',
+    nextArrow: '.rightArrow',
+    centerMode: true,
+  });
+
+
+  function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
